f52a856058a37658aff212b5492910662f96da8d
Nicolas Lopes <nicolasltb123@gmail.com>
2021-05-26 19:56:44 -0300

    added modal and post method

--- public/stylesheets/home_function.js
+++ public/stylesheets/home_function.js
@@ -1,37 +1,50 @@
 let ButtonEl = document.getElementById("btt3");
 let InputEl = document.getElementById("ip");
+const regex = /^([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3}).([0-9]{1,3})/
+// [{"ip_address": "176.178.13.16"}, {"ip_address": "10.0.2.36"}]
 
-ButtonEl.addEventListener('click', () => {
-    let data = InputEl.value;
-    fetch("http://localhost:5000/post_ip", {
-        method: "POST",
-        body: JSON.stringify({ip_address: data})
+ButtonEl.addEventListener('click', async () => {
+
+  let data = InputEl.value;
+
+  if (!data.startsWith('[')) {
+
+    await fetch("http://localhost:5000/post_ip", {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ ip_address: data })
     }).then(res => {
-        console.log("Request complete! response:", res);
+      alert("Request complete! Ip successfully registered.");
+      console.log(res);
     });
+  } else {
+
+    await fetch("http://localhost:5000/post_ip", {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: data
+    }).then(res => {
+      alert("Request complete! Ips successfully registered.");
+      console.log(res);
+    });
+  }
+
+  InputEl.value = '';
 });
 
-// Get the modal
 let modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
 let btn = document.getElementById("bttModal");
-
-// Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
